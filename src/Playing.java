1a8ea1db6837490efc1c87c28db28eaf8c977785
Karen <karmahucents@gmail.com>
2024-01-22 16:27:57 -0500

    add agregando playing para empezar

---
+++ src/Playing.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Playing {
+
+    public static void main(String[] args){
+
+        Scanner er = new Scanner(System.in);
+
+        System.out.println("--- BIENVENIDO ---");
+        System.out.println("Por favor ingrese su nombre: ");
+        String name = er.nextLine();
+        System.out.println("Bienvenido " + name);
+        System.out.println("Elige un número del 1 al 100");
+    }
+}
