6cb0ef62afa724e4cf45020b2da688367bbec1b5
Karen <karmahucents@gmail.com>
2024-01-22 10:59:09 -0500

    add añadiendo pruebas en la consola

---
+++ src/Prueba.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Prueba {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor ingrese su nombre: ");
+        String name = sc.nextLine();
+        System.out.println("Bienvenido " + name + ". Porfavor ingrese su edad");
+        int age = sc.nextInt();
+        System.out.println("Gracias " + name + " en 10 años usted tendrá " + (age + 10) + " años.");
+    }
+}
