0644c9ca73f58950ff5c78e528fb27eac9191937
Karen <karmahucents@gmail.com>
2024-01-31 16:35:31 -0500

    add añadiendo test para las tres clases de player

---
+++ src/test/HumanPlayerTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import main.HumanPlayer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class HumanPlayerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void makeGuess() {
+    }
+
+    @Test
+    public void testMakeGuess() {
+
+        HumanPlayer humanPlayer = new HumanPlayer("TestPlayer");
+        Scanner scanner = mock(Scanner.class);
+
+        when(scanner.nextLine()).thenReturn("50");
+
+        // Llamar al método makeGuess
+        int guess = humanPlayer.makeGuess();
+
+        // Verificar que el resultado
+        assertEquals(50, guess);
+    }
+
+}
